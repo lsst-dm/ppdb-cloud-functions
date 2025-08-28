bdd3edee9b4780d289b5660f196a6f6f7c568df4
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-08-28 14:33:22 -0700

    Delete scheduled run when tearing down

--- promote_chunks/teardown.sh
+++ promote_chunks/teardown.sh
@@ -7,6 +7,11 @@ if [ -z "${GCP_REGION:-}" ]; then
   exit 1
 fi
 
+# Delete the promote chunks function
 gcloud functions delete promote-chunks \
   --gen2 \
   --region=$GCP_REGION
+
+# Deleted scheduled run
+gcloud scheduler jobs delete promote-chunks-daily \
+  --location=us-central1
