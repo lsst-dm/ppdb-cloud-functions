6d1b3bb7b21ffe8e47a378c05b6d51db91fb6ff5
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-08-29 15:06:37 -0700

    Do not change SA or UA

--- promote_chunks/test-call.sh
+++ promote_chunks/test-call.sh
@@ -3,20 +3,14 @@ set -euxo pipefail
 
 FUNC="promote-chunks"
 
-# 1. Get the function's URL
+# Get the function's URL
 URL="$(gcloud functions describe "$FUNC" --gen2 --region="$GCP_REGION" \
   --format='value(serviceConfig.uri)')"
 
-# 2. Login as the SA
-gcloud config set account ${SERVICE_ACCOUNT_EMAIL}
-
-# 3. Set the environment variable for the dev token
+# Set the environment variable for the dev token
 TOKEN="$(gcloud auth print-identity-token --audiences="$URL")"
 
-# 4. Invoke the function with the dev token
+# Invoke the function with the dev token
 curl -X POST "$URL" \
   -H "Authorization: Bearer ${TOKEN}" \
   -H "Content-Type: application/json" 
-
-# 5. Restore the user account after testing
-gcloud config set account ${USER_EMAIL}
