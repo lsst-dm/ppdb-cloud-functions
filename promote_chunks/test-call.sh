2b6d603589333d662d297b275e6d3c07e7f8d63e
Jeremy McCormick <jeremym@slac.stanford.edu>
2026-03-24 16:06:40 -0500

    Update promotion test script

--- promote_chunks/test-call.sh
+++ promote_chunks/test-call.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-FUNC="promote-chunks"
+URL=$(gcloud functions describe promote-chunks \
+  --region=us-central1 \
+  --gen2 \
+  --format="value(serviceConfig.uri)")
 
-# Get the function's URL
-URL="$(gcloud functions describe "$FUNC" --gen2 --region="$GCP_REGION" \
-  --format='value(serviceConfig.uri)')"
+echo "Calling promote-chunks function at: $URL"
 
-# Set the environment variable for the dev token
-TOKEN="$(gcloud auth print-identity-token --audiences="$URL")"
-
-# Invoke the function with the dev token
 curl -X POST "$URL" \
-  -H "Authorization: Bearer ${TOKEN}" \
-  -H "Content-Type: application/json" 
+  -H "Authorization: Bearer $(gcloud auth print-identity-token)"
