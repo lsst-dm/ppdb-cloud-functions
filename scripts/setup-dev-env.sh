00e75f97929cfaa3c97a3ef6b005268811dadeab
Jeremy McCormick <jeremym@slac.stanford.edu>
2026-08-11 14:35:09 -0500

    Add dev setup script

---
+++ scripts/setup-dev-env.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+########################################################################
+# Development environment variables for the PPDB Google Cloud project. #
+########################################################################
+
+# Basic development environment settings
+export ENVIRONMENT=dev
+export PROJECT_NAME=ppdb-dev
+export PROJECT_ID=${PROJECT_NAME}-5c07
+export REGION=us-central1
+export LOG_LEVEL=DEBUG
+export TIME_ZONE=America/Santiago
