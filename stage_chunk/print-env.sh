1cf1c815547c6ed1814f29541e45be3f6617babc
Jeremy McCormick <jeremym@slac.stanford.edu>
2026-08-11 14:39:10 -0500

    Delete outdated scripts and configuration files
    These have all been replaced by Terraform configuration and GitHub
    actions.

--- stage_chunk/print-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-gcloud functions describe trigger-stage-chunk \
-  --quiet \
-  --gen2 \
-  --region="$GCP_REGION" \
-  --format='json(serviceConfig.environmentVariables)'
--- track_chunk/print-env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-gcloud functions describe track-chunk \
-  --gen2 --region="$GCP_REGION" --format='json(serviceConfig.environmentVariables)'
